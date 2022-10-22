cbf3c2b92aa014171bd3626e4226bec01bb42742
VenkatKunaparaju <venkatkrox@gmail.com>
2022-10-22 18:29:43 -0400

    Loaded tables

---
+++ src/SQLCommands.py
@@ -0,0 +1,58 @@
+createInventory = """
+    CREATE TABLE Inventory (
+        Name VARCHAR(255), Count INT,
+        PRIMARY KEY (Name)
+    )
+    """
+createEmployees = """
+    CREATE TABLE Employees (
+        Employee_ID INT, Name VARCHAR(255), Salary INT,
+        Job VARCHAR(255),
+        PRIMARY KEY (Employee_ID)
+    )
+    """
+createCustomer = """
+    CREATE TABLE Customer (
+        Customer_ID INT, Name VARCHAR(255),
+        Place_in_line INT, Order_ID INT,
+        PRIMARY KEY (Customer_ID)
+    ) 
+    """
+createProtein = """
+    CREATE TABLE Protein (
+        Name VARCHAR(255), Volume INT, Calories INT,
+        Fat INT, Price INT, Extra_price INT,
+        PRIMARY KEY (Name)
+    )
+    """
+createIngredient = """
+    CREATE TABLE Ingredient (
+        Name VARCHAR(255), Volume INT, Calories INT,
+        Fat INT, Price INT, Extra_price INT,
+        PRIMARY KEY (Name)
+    )
+    """
+createOrder = """
+    CREATE TABLE User_order (
+        Order_ID INT, Customer_ID INT, Employee_ID INT,
+        Name VARCHAR(255), Protein_list_ID INT, Ingredient_list_ID INT,
+        PRIMARY KEY (Order_ID)
+    )
+    """
+createMenu = """
+    CREATE TABLE Menu (
+        Name VARCHAR(255), Protein_list_ID INT, Ingredient_list_ID INT,
+        PRIMARY KEY (NAME)
+    )
+    """
+createProteinList = """
+    CREATE TABLE Protein_list (
+        Protein_list_ID INT, Name VARCHAR(255)
+    )
+    """
+createIngredientList = """
+    CREATE TABLE Ingredient_list (
+        Ingredient_list_ID INT, Name VARCHAR(255)
+    )
+    """
+
--- src/load.py
+++ src/load.py
@@ -9,18 +9,25 @@ mydb = mysql.connector.connect( # establish connection
 
 
 cursor = mydb.cursor(buffered=True)
+cursor.execute("DROP DATABASE Chipotle_Lite") #Remove once done with initializing database
 
-
-#Check if database 'Chipotle-Lite' exists
-cursor.execute("DROP DATABASE Chipotle_Lite")
+#Check if database 'Chipotle_Lite' exists
 cursor.execute("SHOW DATABASES") 
 
 check = ""
 for x in cursor:
     check += x[0];
 
-#Create 'Chipotle' if it doesn't exist and load tables and data into it
+#Create 'Chipotle_Lite' if it doesn't exist and load tables and data into it
 if 'Chipotle_Lite' not in check:
     cursor.execute("Create DATABASE Chipotle_Lite")
     cursor.execute("USE Chipotle_Lite")
-    
+    cursor.execute(commands.createInventory)
+    cursor.execute(commands.createEmployees)
+    cursor.execute(commands.createCustomer)
+    cursor.execute(commands.createProtein)
+    cursor.execute(commands.createIngredient)
+    cursor.execute(commands.createOrder)
+    cursor.execute(commands.createMenu)
+    cursor.execute(commands.createProteinList)
+    cursor.execute(commands.createIngredientList);
