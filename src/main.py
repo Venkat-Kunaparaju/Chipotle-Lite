182ac234806398f70a00abc2b297bddda61b3e18
VenkatKunaparaju <venkatkrox@gmail.com>
2022-10-31 18:06:42 -0400

    main

--- src/main.py
+++ src/main.py
@@ -1,12 +1,13 @@
 from load import *
+from setup import *
 
 print(welcomeString)
 print(loginString)
 choice = input()
-if (choice == 1) {
+if (choice == 1):
+    pass
     #To be implemented
-}
-if (choice == 2) {
+if (choice == 2):
+    pass
     #Ui stuff goes here
-}
 
--- src/setup.py
+++ src/setup.py
@@ -1,2 +1,2 @@
 welcomeString = "Welcome to Chipotle Lite!\n";
-loginString = "Are you a customer? (1)\nAre you an employee(2)?\n";
+loginString = "Are you a customer? (1)\nAre you an employee? (2)\n";
